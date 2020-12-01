94995d82105b1dc0ea6f8ce1ed7c451cdbc08f08
Tasha Kim <jae_young_kim@brown.edu>
2020-12-01 12:54:58 -0400

    Complete form submission page, follow styling guideline

--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
+var path = require("path");
 
-router.use(express.urlencoded({
-    extended: true
-  }))
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/html/', 'contact.html'));
-})
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/html/", "contact.html"));
+});
 
-router.post('/submit-form', function(req, res) {
-    res.send("Form submitted!");
-})
+router.post("/submit-form", function (req, res) {
+  Email.send({
+    To: "swe@yourdomain.com",
+    From: req.body.email,
+    Subject: "Email from " + req.body.name,
+    Body: req.body.message,
+  }).then((message) => alert(message));
+  console.log("mail was sent from" + req.body.name);
+  res.send("Form submitted!");
+});
 
 module.exports = router;
